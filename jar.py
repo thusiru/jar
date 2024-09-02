bf16ba43a5734e5eb1558624350b9d7b105aec93
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-03 01:18:13 +0530

    Error handlings location changes
    Error handlings location change to jar function

--- jar.py
+++ jar.py
@@ -2,16 +2,17 @@ import sys
 
 
 def main():
-    try:
-        size = int(input("Size for the jar? "))
-    except ValueError:
-        sys.exit("Value can't be a string")
-    if size < 0:
-        sys.exit("Size can't be negetive")
+    size = input("Size for the jar? ")
     print(jar(size))
 
 
 def jar(size):
+    try:
+        size = int(size)
+    except ValueError:
+        sys.exit("Size can't be a string")
+    if size < 0:
+        sys.exit("Size can't be negative")
     return f"Okay, I created a size {size} jar!"
     
 
