71e0be4c4a800876a2589cef7244539ece6d5cea
Mona Lisa <147712158+thusiru@users.noreply.github.com>
2024-09-03 02:16:12 +0530

    Update jar.py
    remove sys.exit
    update sys.exit to print invalid input and ask the user for a input again

--- jar.py
+++ jar.py
@@ -1,18 +1,20 @@
-import sys
-
-
 def main():
     size = input("Size for the jar? ")
     print(jar(size))
 
 
 def jar(size):
-    try:
-        size = int(size)
-    except ValueError:
-        sys.exit("Size can't be a string")
-    if size < 0:
-        sys.exit("Size can't be negative")
+    while True:
+        try:
+            size = int(size)
+        except ValueError:
+            return "Size cannot be a string"
+        else:
+            if size < 0:
+                return "Size cannot be negative"
+            else:
+                break
+
     return f"Okay, I created a size {size} jar!"
     
 
